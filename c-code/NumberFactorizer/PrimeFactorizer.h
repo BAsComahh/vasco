e5471380552e597b64312517d5b1cfd1480576b2
BasComahh <3160756128@qq.com>
2024-12-24 12:14:38 +0800

    blabla

---
+++ c-code/NumberFactorizer/PrimeFactorizer.h
@@ -0,0 +1,6 @@
+
+
+int gcd(int number1, int number2);
+int primeFactorize(int number);
+
+int* getFactors(int num);
---
+++ c-code/NumberFactorizer/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "PrimeFactorizer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int in = 0;
+    scanf("%d",&in);
+    printf("ans = %d\n",primeFactorize(in));
+    return 0;
+}
